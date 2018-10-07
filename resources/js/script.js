fc6774927dbbe58c71c556128ca78a5fba6c4bc5
Ashwin Chatterji <ashwinath@hotmail.com>
2018-10-07 19:56:59 +0800

    nav bar hamburger

--- resources/js/script.js
+++ resources/js/script.js
@@ -65,4 +65,19 @@ $(document).ready(function() {
     $(".js--wp-4").waypoint(function(direction) {
         $(".js--wp-4").addClass("animated pulse");
     }, { offset: "60%" });
+
+    /* mobile nav */
+    $(".js--nav-icon").click(function() {
+        var nav = $(".js--main-nav");
+        nav.slideToggle(200);
+
+        var icon = $(".js--nav-icon i");
+        if (icon.hasClass("ion-navicon-round")) {
+            icon.addClass("ion-close-round");
+            icon.removeClass("ion-navicon-round");
+        } else {
+            icon.addClass("ion-navicon-round");
+            icon.removeClass("ion-close-round");
+        }
+    });
 });
